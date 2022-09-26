cc6ddd691c46bdc6e9b85db9a59a0278c28a9ff3
Julia-Cabrera <114102352+Julia-Cabrera@users.noreply.github.com>
2022-09-25 20:43:23 -0500

    Add files via upload

---
+++ TIC/TIC/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace TIC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Insertar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Insertar
+            // 
+            this.btn_Insertar.Location = new System.Drawing.Point(108, 50);
+            this.btn_Insertar.Name = "btn_Insertar";
+            this.btn_Insertar.Size = new System.Drawing.Size(108, 28);
+            this.btn_Insertar.TabIndex = 0;
+            this.btn_Insertar.Text = "Ingresar";
+            this.btn_Insertar.UseVisualStyleBackColor = true;
+            this.btn_Insertar.Click += new System.EventHandler(this.btn_Insertar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(562, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(479, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(191, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(448, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(108, 84);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(562, 147);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(108, 249);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(562, 150);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 416);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_Insertar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Insertar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ TIC/TIC/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TIC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Insertar_Click(object sender, EventArgs e)
+        {
+            int registrosAfectados = 0;
+            registrosAfectados = Conexion.EjecutarConsulta(textBox1.Text);
+            Mensajes.Mensaje("Registros Afectados: "+registrosAfectados);
+            LlenarCombo(textBox1.Text, comboBox1,"id","Nombre");
+            LlenarListView(textBox1.Text, listView1);
+            LlenarDataGrid(textBox1.Text, dataGridView1);
+        }
+        public static void LlenarCombo(string consulta, ComboBox combo, String id, String campo)
+        {
+            DataTable dt;
+            //  dt = Conexion.EjecutaSeleccion(consulta);
+            dt = Conexion.AgregarElementoCombo(consulta);
+            if (dt == null)
+            {
+                return;
+            }
+            combo.Items.Clear();
+            combo.DataSource = dt;
+            combo.ValueMember = id;
+            combo.DisplayMember = campo;
+
+        }
+        public static void LlenarDataGrid(string consulta, DataGridView Data)
+        {
+
+            DataTable dt;
+            dt = Conexion.EjecutaSeleccion(consulta);
+            Data.DataSource = dt;
+        }
+        public static void LlenarListView(string consulta, ListView list)
+        {
+            DataTable dt;
+            dt = Conexion.EjecutaSeleccion(consulta);
+            int CantidadC = dt.Columns.Count;
+            list.Items.Clear();
+            list.View = View.Details;
+            list.GridLines = true;
+            for (int i = 0; i<CantidadC; i++)
+            {
+                list.Columns.Add(dt.Columns[i].ToString());
+            }
+            foreach (DataRow renglon in dt.Rows)
+            {
+                String[] arreglo = new String[CantidadC];
+                ListViewItem elemento = new ListViewItem();
+                for(int i = 0; i<CantidadC; i++)
+                {
+                    arreglo[i] = renglon[i].ToString();
+                    elemento = new ListViewItem(arreglo);
+                }
+                list.Items.Add(elemento);
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = comboBox1.SelectedValue.ToString();
+        }
+    }
+}
